46ce8379282e4d15ea2b3ef902893b29a9d57293
Cui <sandy881133@gmail.com>
2026-07-26 10:33:01 +0800

    Cui MVC Product

---
+++ CLOthings/ViewModels/ProductViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CLOthings.ViewModels
+{
+    public class ProductViewModel
+    {
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage ="此欄位必填")]
+        [Display(Name = "商品名稱")]
+        public string ProductName { get; set; }
+
+        [Required(ErrorMessage ="此欄位必填")]
+        [Display(Name = "供應商")]
+        public int? SupplierId { get; set; }
+
+        [Required(ErrorMessage = "此欄位必填")]
+        [Display(Name = "商品分類")]
+        public int? ProductCategoryId { get; set; }
+
+        //[Display(Name = "商品分類")]
+        //public string ProductCategoryName { get; set; }
+
+        [Display(Name = "商品描述")]
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "此欄位必填")]
+        [Display(Name = "價格")]
+        public decimal? Price { get; set; }
+
+        [Required(ErrorMessage = "此欄位必填")]
+        [Display(Name = "上架狀態")]
+        public string Status { get; set; }
+
+        [Display(Name = "折扣開始日期")]
+        public DateTime? SalesStartDate { get; set; }
+
+        [Display(Name = "折扣結束日期")]
+        public DateTime? SalesEndDate { get; set; }
+
+        [Display(Name = "商品圖片")]
+        public List<IFormFile>? ProductImg { get; set; }
+
+        [Display(Name = "商品規格")]
+        public List<ProductSpecificationViewModel>? ProductSpecifications { get; set; }
+    }
+
+    public class ProductSpecificationViewModel
+    {
+        [Required(ErrorMessage = "此欄位必填")]
+        [Display(Name = "尺寸")]
+        public string? Size { get; set; }
+
+        [Required(ErrorMessage = "此欄位必填")]
+        [Display(Name = "顏色")]
+        public string? Color { get; set; }
+
+        [Required(ErrorMessage = "此欄位必填")]
+        [Display(Name = "庫存數量")]
+        public int? Inventory { get; set; }
+
+        [Display(Name = "在途庫存")]
+        public int UnitOnOrder { get; set; }
+
+        [Required(ErrorMessage = "此欄位必填")]
+        [Display(Name = "再訂量")]
+        public int? ReorderLevel { get; set; }
+
+    }
+}
